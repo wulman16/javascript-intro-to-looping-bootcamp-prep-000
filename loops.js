c9712877316cf1321d5a2e9659c5d9d7df6c2e69
William Ulman <will.ulman@gmail.com>
2019-01-04 00:59:53 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,8 +1,8 @@
 function forLoop(arr) {
   for(let i = 0; i < 25; i++) {
     if (i === 1) {
-      arr += 'I am 1 strange loop.';
-    } else arr += `I am ${i} strange loops.`;
+      arr.push('I am 1 strange loop.');
+    } else arr.push(`I am ${i} strange loops.`);
   }
   return arr;
 }
